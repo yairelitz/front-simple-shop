3d8ab005891f79e9e6e56cfa10ddd29a82d6662b
yairelitz <yair99eli@gmail.com>
2026-08-26 21:55:04 +0300

    add GitHub pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/front-simple-shop/',
+
   server: {
-  port: 3000,
-  strictPort: true
-}
+    port: 3000,
+    strictPort: true
+  }
 })
